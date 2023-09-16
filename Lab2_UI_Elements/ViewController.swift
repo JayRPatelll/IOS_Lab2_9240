30cb6085483fa6c904f834c1815d63b9c4c9d54d
JayRPatelll <jaypatel9779ca@gmail.com>
2023-09-16 10:46:38 -0400

    Initial Commit

---
+++ Lab2_UI_Elements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2_UI_Elements
+//
+//  Created by user232336 on 9/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
