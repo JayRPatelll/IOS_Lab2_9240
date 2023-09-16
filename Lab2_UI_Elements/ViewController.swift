1e28048cfea3ad159eb3882c39e4e3a0ad220d09
JayRPatelll <jaypatel9779ca@gmail.com>
2023-09-16 12:52:33 -0400

    Lab 2 UI Elements for Practice

--- Lab2_UI_Elements/ViewController.swift
+++ Lab2_UI_Elements/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        printIt()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func plusButton(_ sender: Any) {
+        counter+=1
+        printIt()
+    }
+    
+    
+    @IBAction func minusButton(_ sender: Any) {
+        counter-=1
+        printIt()
+    }
+    func printIt(){
+        counterLabel.text = String(counter)
+        
+    }
+    
 }
 
